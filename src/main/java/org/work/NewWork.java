c3b1809804bf21a20902d4461b12a08a20d12637
vickychan007 <vigneshg007@yahoo.com>
2021-12-25 16:29:07 +0530

    2021 code

--- src/main/java/org/work/NewWork.java
+++ src/main/java/org/work/NewWork.java
@@ -6,8 +6,8 @@ public class NewWork {
 	
 	@Test
 	private void test1() {
-		System.out.println("Name:Vignesh");
-		System.out.println("Age: 28yrs");
+		System.out.println("Year:2021");
+		System.out.println("Month: 25th december");
 
 	}
 
